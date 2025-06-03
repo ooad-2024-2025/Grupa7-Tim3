baf338bcaa9c105a1aba6e84653e260ed3f490d1
Adna Bajramović <abajramovi2@etf.unsa.ba>
2025-06-03 16:34:41 +0200

    Registracija prosireno

--- Booking/obj/Debug/net8.0/Booking.AssemblyInfo.cs
+++ Booking/obj/Debug/net8.0/Booking.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Booking")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ae8398be2b0b028697658dd0c38db1358e7356a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e05e77b50dc34fa298e22ead10caa31098c64bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Booking")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Booking")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
